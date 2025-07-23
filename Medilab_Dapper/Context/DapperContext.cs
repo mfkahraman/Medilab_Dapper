5a061cb35f52e6d4e7f61f4ed8058e88c00a6dc6
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-23 14:53:16 +0300

    Department dtos, appsettings connection and context created

---
+++ Medilab_Dapper/Context/DapperContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Medilab_Dapper.Context
+{
+    public class DapperContext
+    {
+        private readonly string _connectionString;
+        private readonly IConfiguration _configuration;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = configuration.GetConnectionString("SqlConnection");
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
---
+++ Medilab_Dapper/Dtos/DepartmentDtos/CreateDepartmentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Medilab_Dapper.Dtos.DepartmentDtos
+{
+    public class CreateDepartmentDto
+    {
+        public required string DepartmentName { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
