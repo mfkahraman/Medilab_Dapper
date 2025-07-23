7a2f7d4004637d8cfc9076fc849be2a4a62f5896
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-23 14:35:53 +0300

    Add project files.

---
+++ Medilab_Dapper/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Medilab_Dapper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
