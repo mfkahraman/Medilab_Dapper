1d3b2b104a7e1c89fb596ed7867cf5a541b5e99b
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-23 14:57:22 +0300

    IRepository created

---
+++ Medilab_Dapper/Dtos/DepartmentDtos/GetDepartmentByIdDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Medilab_Dapper.Dtos.DepartmentDtos
+{
+    public class GetDepartmentByIdDto
+    {
+        public required int DepartmentId { get; set; }
+        public required string DepartmentName { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
---
+++ Medilab_Dapper/Repositories/DepartmentRepository/IDepartmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Medilab_Dapper.Dtos.DepartmentDtos;
+
+namespace Medilab_Dapper.Repositories.DepartmentRepository
+{
+    public interface IDepartmentRepository
+    {
+        Task<IEnumerable<ResultDepartmentDto>> GetAllDepartmentsAsync();
+        Task<GetDepartmentByIdDto> GetDepartmentByIdAsync(int id);
+
+        Task CreateDepartmentAsync(CreateDepartmentDto createDto);
+        Task UpdateDepartmentAsync(UpdateDepartmentDto updateDto);
+        Task DeleteDepartmentAsync(int id);
+    }
+}
