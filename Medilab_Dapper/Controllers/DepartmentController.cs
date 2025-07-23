450bc27070fa21105eb889222dbaeac837f1c930
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-23 15:53:18 +0300

    Add Department management features and views
    - Added `Microsoft.VisualStudio.Web.CodeGeneration.Design` package.
    - Updated `_ViewImports.cshtml` with new namespaces and TagHelper.
    - Implemented `DepartmentController` for CRUD operations.
    - Created `CreateDepartmentDto` model and corresponding view.
    - Enhanced `Index.cshtml` to list departments with update/delete options.

---
+++ Medilab_Dapper/Controllers/DepartmentController.cs
@@ -0,0 +1,63 @@
+﻿using Medilab_Dapper.Dtos.DepartmentDtos;
+using Medilab_Dapper.Repositories.DepartmentRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Medilab_Dapper.Controllers
+{
+    public class DepartmentController(IDepartmentRepository repository) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var departments = await repository.GetAllDepartmentsAsync();
+            return View(departments);
+        }
+
+        public IActionResult CreateDepartment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDepartment(CreateDepartmentDto createDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(createDto);
+            }
+
+            await repository.CreateDepartmentAsync(createDto);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> UpdateDepartment(int id)
+        {
+            var department = await repository.GetDepartmentByIdAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateDepartment(UpdateDepartmentDto updateDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(updateDto);
+            }
+            await repository.UpdateDepartmentAsync(updateDto);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DeleteDepartment(int id)
+        {
+            var department = await repository.GetDepartmentByIdAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+    }
+}
