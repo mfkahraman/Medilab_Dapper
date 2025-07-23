0593510830e2a1e61d8b2aa098ae4b315941f021
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-23 15:37:55 +0300

    ServiceRegistrations class created, services are registrated

---
+++ Medilab_Dapper/Extensions/ServiceRegistirations.cs
@@ -0,0 +1,18 @@
+﻿using Medilab_Dapper.Context;
+using Medilab_Dapper.Repositories.DepartmentRepository;
+
+namespace Medilab_Dapper.Extensions
+{
+    public static class ServiceRegistirations
+    {
+
+        public static IServiceCollection AddRepositoriesExt(this IServiceCollection services)
+        {
+            services.AddScoped<DapperContext>();
+
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+
+            return services;
+        }
+    }
+}
--- Medilab_Dapper/Program.cs
+++ Medilab_Dapper/Program.cs
@@ -1,6 +1,13 @@
+using Medilab_Dapper.Context;
+using Medilab_Dapper.Extensions;
+using Medilab_Dapper.Repositories.DepartmentRepository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddRepositoriesExt();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
