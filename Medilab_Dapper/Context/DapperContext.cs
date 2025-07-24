30701b1d1191afca21481636bc899f5002ce1be5
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-24 14:20:21 +0300

    warning fixed

--- Medilab_Dapper/Context/DapperContext.cs
+++ Medilab_Dapper/Context/DapperContext.cs
@@ -3,15 +3,10 @@ using System.Data;
 
 namespace Medilab_Dapper.Context
 {
-    public class DapperContext
+    public class DapperContext(IConfiguration configuration)
     {
-        private readonly string _connectionString;
-
-        public DapperContext(IConfiguration configuration)
-        {
-            _connectionString = configuration.GetConnectionString("SqlConnection") 
-                ?? throw new ArgumentNullException(nameof(configuration), "Connection string cannot be null.");
-        }
+        private readonly string _connectionString = configuration.GetConnectionString("SqlConnection") 
+            ?? throw new ArgumentNullException(nameof(configuration), "Connection string cannot be null.");
 
         public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
     }
