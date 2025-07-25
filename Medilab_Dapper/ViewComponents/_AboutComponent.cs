14a5607dada9e3c9810c4b0263465f73a6b0a887
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-25 14:58:22 +0300

    About and stats components created

---
+++ Medilab_Dapper/ViewComponents/_AboutComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Medilab_Dapper.ViewComponents
+{
+    public class _AboutComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
