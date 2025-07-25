94e0c2ca72869ad7e0e5371a3d76e4fb3e2ed2ea
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-25 15:13:09 +0300

    Departments, doctors, faq and testimonials components created

---
+++ Medilab_Dapper/ViewComponents/_DoctorsComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Medilab_Dapper.ViewComponents
+{
+    public class _DoctorsComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Medilab_Dapper/ViewComponents/_TestimonialsComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Medilab_Dapper.ViewComponents
+{
+    public class _TestimonialsComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
