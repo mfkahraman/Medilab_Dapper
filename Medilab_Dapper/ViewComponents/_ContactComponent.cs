01a359e5f4c50c84c2da2f8f393770569d2b585b
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-25 15:16:36 +0300

    Contact and gallery components created

---
+++ Medilab_Dapper/ViewComponents/_ContactComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Medilab_Dapper.ViewComponents
+{
+    public class _ContactComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
