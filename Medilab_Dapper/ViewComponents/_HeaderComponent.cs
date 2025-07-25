41c9304db806e71a736093f2e11e58bd12988558
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-25 14:46:57 +0300

    Header component created

---
+++ Medilab_Dapper/ViewComponents/_HeaderComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Medilab_Dapper.ViewComponents
+{
+    public class _HeaderComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
