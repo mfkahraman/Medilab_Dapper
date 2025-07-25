2214f319e2ec4648314906ea598503a3e2e77408
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-25 14:52:44 +0300

    Hero component created

---
+++ Medilab_Dapper/ViewComponents/_HeroComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Medilab_Dapper.ViewComponents
+{
+    public class _HeroComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
