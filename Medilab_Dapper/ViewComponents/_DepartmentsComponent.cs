45a6a3560ba7d1dd0d1ef5ad26d7208268fa2f1c
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-30 16:12:12 +0300

    Departments component fetching datas from db dynamycally

--- Medilab_Dapper/ViewComponents/_DepartmentsComponent.cs
+++ Medilab_Dapper/ViewComponents/_DepartmentsComponent.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Medilab_Dapper.Repositories.DepartmentRepository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Medilab_Dapper.ViewComponents
 {
-    public class _DepartmentsComponent : ViewComponent
+    public class _DepartmentsComponent(IDepartmentRepository repository) : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var departments = await repository.GetAllDepartmentsAsync();
+            if (departments == null || !departments.Any())
+            {
+                return Content("No departments available.");
+            }
+            List<int> ids = new() { 1, 4, 6, 7, 8 };
+            var filteredDepartments = departments.Where(d => ids.Contains(d.DepartmentId)).ToList();
+
+            if (!filteredDepartments.Any())
+            {
+                return Content("No matching departments available.");
+            }
+
+            return View(filteredDepartments);
         }
     }
 }
