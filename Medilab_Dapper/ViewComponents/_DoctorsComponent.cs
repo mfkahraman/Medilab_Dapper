5d01cd423aa2478100855b612eda1ff4c8262560
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-30 16:21:36 +0300

    Doctors are dynamycally fetching from db

--- Medilab_Dapper/ViewComponents/_DoctorsComponent.cs
+++ Medilab_Dapper/ViewComponents/_DoctorsComponent.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Medilab_Dapper.Repositories.DoctorRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Medilab_Dapper.ViewComponents
 {
-    public class _DoctorsComponent : ViewComponent
+    public class _DoctorsComponent(IDoctorRepository repository) : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var doctors = (await repository.GetDoctorsWithDepartmentAsync()).Take(4).ToList();
+            if (doctors == null || !doctors.Any())
+            {
+                return Content("No doctors available.");
+            }
+            return View(doctors);
         }
     }
 }
