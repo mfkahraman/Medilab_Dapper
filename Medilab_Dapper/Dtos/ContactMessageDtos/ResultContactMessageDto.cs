9a8c33045227fc9775043e4f5b1804cde80d5c9f
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-07-30 16:55:18 +0300

    ContactMessage Dtos created

---
+++ Medilab_Dapper/Dtos/ContactMessageDtos/ResultContactMessageDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Medilab_Dapper.Dtos.ContactMessageDtos
+{
+    public class ResultContactMessageDto
+    {
+        public int Id { get; set; }
+        public required string FullName { get; set; }
+        public required string Email { get; set; }
+        public required string Subject { get; set; }
+        public required string Message { get; set; }
+        public bool IsRead { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
